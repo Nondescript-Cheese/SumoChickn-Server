a57ee342415e30e5f5a24233e73030f1e89b49fe
Michael Cheung <michaelc1993@gmail.com>
2016-03-29 23:49:35 -0700

    create a route that will allow users to vote on challenge proof

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,9 +1,11 @@
 var auth = require('./authController.js');
 var users = require('./usersController.js');
 var challenges = require('./challengesController.js');
+var proof = require('./proofController.js');
 
 module.exports = {
   auth: auth,
   users: users,
-  challenges: challenges
+  challenges: challenges,
+  proof: proof
 };
