c46ea6e7366ab5b62fb9e9a921959cbdfba9b39b
Michael Cheung <michaelc1993@gmail.com>
2016-03-30 12:19:10 -0700

    add comments for getUserInfo

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -4,17 +4,20 @@ module.exports = {
 
   getUserInfo: function(req, res) {
     var userId = parseInt(req.params.userID);
-    return db.models.User.findAll({
+    //get a user from the database specified by id
+    return db.models.User.find({
       where: {
         id: userId
       }
     }).then(function(data) {
+      //get all challenges associated with the specific user
       return db.models.Challenge.findAll({
         where: {
           UserId: userId
         }
       });
     }).then(function(challenges) {
+      //send the associated challenges to the client
       res.send(200, challenges);
     }).catch(function(err) {
       res.send(404, 'error getting info');
@@ -26,5 +29,5 @@ module.exports = {
       res.send(200, data);
     });
   }
-  
+
 };
